58d442a1c7f5d2e45c3874e12d7c8024ff527687
Conent <albertocontento@gmail.com>
2015-05-16 22:43:57 +0100

    dio can

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,7 +29,7 @@ class PagesController < ApplicationController
 	      @string = "Recived"
 	      #provare a commentare la linea sotto
 	      #render :json =>{:data => @string}
-	      puts response :layout => false
+	      puts response {render :layout => false}
     end
    end
 
