2add75057e1726ba5c72b51ac3157f63271f99f4
Conent <albertocontento@gmail.com>
2015-06-05 14:19:25 +0100

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get "test" => "pages#test"
   
   post "testsend" => "pages#testsend"
+
+  post "mobilelogin" => "mobilecontroller#login"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
